5d0dd0a082184863dc5244abf873d5bc049cb93c
Jerome Meyer <meyer.jerome@gmail.com>
2025-01-08 14:48:46 +0100

    Add Postgres connection

--- config/config.go
+++ config/config.go
@@ -6,12 +6,14 @@ import (
 
 type Config struct {
 	Port          string
+	DatabaseURL   string
 	RandomUserAPI string
 }
 
 func NewConfig() *Config {
 	return &Config{
 		Port:          getEnvOrDefault("PORT", "8080"),
+		DatabaseURL:   getEnvOrDefault("DATABASE_URL", "postgres://postgres:postgres@localhost:5432/userdb?sslmode=disable"),
 		RandomUserAPI: getEnvOrDefault("RANDOM_USER_API", "https://randomuser.me/api/"),
 	}
 }
---
+++ database/postgres.go
@@ -0,0 +1,24 @@
+package repository
+
+import (
+	"users-service/model"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type PostgresDB struct {
+	db *gorm.DB
+}
+
+func NewPostgresDB(url string) *PostgresDB {
+	db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	// Auto migrate the schema
+	db.AutoMigrate(&model.User{})
+
+	return &PostgresDB{db: db}
+}
